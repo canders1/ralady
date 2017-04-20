fe8109865b87487bae040654212b4a7d5d41cc75
Carolyn Anderson <canders277@gmail.com>
2017-04-20 18:01:37 -0400

    parsing bible

--- xml_soup.py
+++ xml_soup.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(open("short.xml"), 'xml',from_encoding="utf-8")
+soup = BeautifulSoup(open("pdfminer-master/output.xml"), 'xml',from_encoding="utf-8")
 print soup.original_encoding
-print(soup)
+for line in soup.find_all('textline'):
+	s = ""
+	for l in line.find_all('text'):
+		s = s + l.string
+	print(s.encode('utf-8'))
