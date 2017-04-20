458ab3f9fdf2b1180cf6a8bbe0dc62f6ea9a8450
Carolyn Anderson <canders277@gmail.com>
2017-04-20 17:26:28 -0400

    parsing xml

--- xml_soup.py
+++ xml_soup.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(open("pdfminer-master/output.xml"), 'xml')
-print(soup.prettify())
+soup = BeautifulSoup(open("short.xml"), 'xml',from_encoding="utf-8")
+print soup.original_encoding
+print(soup)
