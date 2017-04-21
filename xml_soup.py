97b6c23d46cd671d730e8fbcf080926c0a28d255
Carolyn Anderson <canders277@gmail.com>
2017-04-21 17:12:13 -0400

    parse chapter names

--- xml_soup.py
+++ xml_soup.py
@@ -1,9 +1,16 @@
 from bs4 import BeautifulSoup
+import re
 
 soup = BeautifulSoup(open("pdfminer-master/output.xml"), 'xml',from_encoding="utf-8")
-print soup.original_encoding
+chapters = []
 for line in soup.find_all('textline'):
 	s = ""
+	prev = ""
 	for l in line.find_all('text'):
 		s = s + l.string
+	pat = re.compile('.*\.\.\..*')
+	if (pat.match(s)):
+		chapters.append(s.split(".")[0])
+	prev = s
 	print(s.encode('utf-8'))
+print chapters
