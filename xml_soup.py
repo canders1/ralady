15bb293b7564207db2fdbec2d46bee8c87b669d3
Carolyn Anderson <canders277@gmail.com>
2017-04-24 16:12:27 -0400

    short bible

--- xml_soup.py
+++ xml_soup.py
@@ -1,16 +1,29 @@
 from bs4 import BeautifulSoup
 import re
 
-soup = BeautifulSoup(open("pdfminer-master/output.xml"), 'xml',from_encoding="utf-8")
+soup = BeautifulSoup(open("pdfminer-master/short.xml"), 'xml',from_encoding="utf-8")
 chapters = []
+intro = 0
+pat = re.compile('.*\.\.\..*')
 for line in soup.find_all('textline'):
 	s = ""
 	prev = ""
 	for l in line.find_all('text'):
 		s = s + l.string
-	pat = re.compile('.*\.\.\..*')
 	if (pat.match(s)):
 		chapters.append(s.split(".")[0])
 	prev = s
+start = chapters[0].upper()
+startpat = re.compile('(\w+) '+start)
+for line in soup.find_all('textline'):
+	s = ""
+	prev = ""
+	for l in line.find_all('text'):
+		s = s + l.string
 	print(s.encode('utf-8'))
-print chapters
+	print '(\w+) '+start
+	if (startpat.match(s)):
+		print "Found start!"
+	prev = s
+	#print(s.encode('utf-8'))
+print start
