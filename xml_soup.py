35ddf9381d296bb336d60ed02524d77ed130ed15
Carolyn Anderson <canders277@gmail.com>
2017-04-25 11:30:03 -0400

    find start

--- xml_soup.py
+++ xml_soup.py
@@ -4,26 +4,23 @@ import re
 soup = BeautifulSoup(open("pdfminer-master/short.xml"), 'xml',from_encoding="utf-8")
 chapters = []
 intro = 0
-pat = re.compile('.*\.\.\..*')
 for line in soup.find_all('textline'):
 	s = ""
-	prev = ""
 	for l in line.find_all('text'):
 		s = s + l.string
-	if (pat.match(s)):
+	if (s.find("....") > -1):
 		chapters.append(s.split(".")[0])
-	prev = s
-start = chapters[0].upper()
-startpat = re.compile('(\w+) '+start)
+start = chapters[0].strip().upper()
+prev = ""
 for line in soup.find_all('textline'):
 	s = ""
-	prev = ""
 	for l in line.find_all('text'):
 		s = s + l.string
-	print(s.encode('utf-8'))
-	print '(\w+) '+start
-	if (startpat.match(s)):
+	if (prev.isupper() and s.isupper()):
+		s = prev+s
+	if (start in s):
 		print "Found start!"
 	prev = s
-	#print(s.encode('utf-8'))
+	if (s.isupper()==False):
+		print(s.encode('utf-8'))
 print start
