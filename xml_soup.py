6e2885a59288f09bb02b14c3790662442cadac7b
Carolyn Anderson <canders277@gmail.com>
2017-04-25 13:13:56 -0400

    parsed into chapters

--- xml_soup.py
+++ xml_soup.py
@@ -2,25 +2,48 @@ from bs4 import BeautifulSoup
 import re
 
 soup = BeautifulSoup(open("pdfminer-master/short.xml"), 'xml',from_encoding="utf-8")
-chapters = []
-intro = 0
+chapters = ['intro']
+introduction = []
+books = {}
+books['intro'] = introduction
 for line in soup.find_all('textline'):
 	s = ""
 	for l in line.find_all('text'):
 		s = s + l.string
 	if (s.find("....") > -1):
 		chapters.append(s.split(".")[0])
-start = chapters[0].strip().upper()
+for title in chapters:
+	books[title] = []
+section = 0
+nextsection = chapters[section+1].strip().upper()
 prev = ""
 for line in soup.find_all('textline'):
+	lines = []
 	s = ""
 	for l in line.find_all('text'):
 		s = s + l.string
-	if (prev.isupper() and s.isupper()):
-		s = prev+s
-	if (start in s):
-		print "Found start!"
-	prev = s
-	if (s.isupper()==False):
+	if (s.isdigit()):
 		print(s.encode('utf-8'))
-print start
+	if (s.isupper()):
+		if (prev.isupper()):
+			prev = prev+s
+		else:
+			prev = s
+	elif (prev.isupper()):
+		lines.append(prev)
+		lines.append(s)
+		prev = s
+	else:
+		lines.append(s)
+		prev = s
+	curBook = books[chapters[section]]
+	for w in lines:
+		if (nextsection in w):
+			section = section+1
+			nextsection = chapters[section+1].strip().upper()
+		if (w):
+			curBook.append(w.encode('utf-8'))
+for k in books.keys():
+	print(k.encode('utf-8'))
+	for h in books[1]:
+		print h
