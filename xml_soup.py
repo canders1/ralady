895d936fe5eaacb0a593df9e693766a5d9567118
Carolyn Anderson <canders277@gmail.com>
2017-04-25 14:16:31 -0400

    identify page numbers and quotes

--- xml_soup.py
+++ xml_soup.py
@@ -1,8 +1,17 @@
+"""
+Parses the San Juan Guelavia Zapotec bible; or more generally, any book with the structure below:
++Introductory material, including a table of contents, where book names are identified by lines containing four periods
+ Example: John ......... 109
++Books with titles in CAPS and chapters that are numbered:
+ Example: 
+ JOHN
+ 1 In the beginning was the word.
+"""
 from bs4 import BeautifulSoup
 import re
 
 soup = BeautifulSoup(open("pdfminer-master/short.xml"), 'xml',from_encoding="utf-8")
-chapters = ['intro']
+titles = ['intro']
 introduction = []
 books = {}
 books['intro'] = introduction
@@ -11,39 +20,52 @@ for line in soup.find_all('textline'):
 	for l in line.find_all('text'):
 		s = s + l.string
 	if (s.find("....") > -1):
-		chapters.append(s.split(".")[0])
-for title in chapters:
+		titles.append(s.split(".")[0])
+for title in titles:
 	books[title] = []
 section = 0
-nextsection = chapters[section+1].strip().upper()
+nextsection = titles[section+1].strip().upper()
 prev = ""
 for line in soup.find_all('textline'):
 	lines = []
 	s = ""
+	prevnum = 0
 	for l in line.find_all('text'):
 		s = s + l.string
-	if (s.isdigit()):
+	q = re.search('(.*:.*)',s)
+	if (s.strip().isdigit()):
+		print "Page number"
+	elif (q):
+		print "Quote!"
 		print(s.encode('utf-8'))
-	if (s.isupper()):
-		if (prev.isupper()):
-			prev = prev+s
-		else:
-			prev = s
-	elif (prev.isupper()):
-		lines.append(prev)
-		lines.append(s)
-		prev = s
 	else:
-		lines.append(s)
-		prev = s
-	curBook = books[chapters[section]]
-	for w in lines:
-		if (nextsection in w):
-			section = section+1
-			nextsection = chapters[section+1].strip().upper()
-		if (w):
-			curBook.append(w.encode('utf-8'))
-for k in books.keys():
-	print(k.encode('utf-8'))
-	for h in books[1]:
-		print h
+		if (s.isupper()):
+			if (prev.isupper()):
+				prev = prev+s
+			else:
+				prev = s
+		elif (prev.isupper()):
+			lines.append(prev)
+			lines.append(s)
+			prev = s
+		else:
+			lines.append(s)
+			prev = s
+		curBook = books[titles[section]]
+		for w in lines:
+			if (nextsection in w):
+				section = section+1
+				nextsection = titles[section+1].strip().upper()
+			if (section > 0):
+				n = re.findall(('[0-9]+'),w)
+				if n:
+					for x in n:
+						if (int(x)==1):
+							print "Found next verse!"
+					print(s.encode('utf-8'))
+			if (w):
+				curBook.append(w.encode('utf-8'))
+#for k in books.keys():
+	#print(k.encode('utf-8'))
+for h in books[titles[1]]:
+	print h
