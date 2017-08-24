9c7add598322ecc10f8d2aaff49a663a2c2502c5
Carolyn Anderson <canders277@gmail.com>
2017-08-24 12:31:34 -0400

    convert definiteness to r script

---
+++ scripts/definiteness.R
@@ -0,0 +1,126 @@
+#Scrapes Pamela Munro's definiteness paper for Zapotec examples
+#Outputs data to csv
+
+quotes <- function(line,qopen,qclose){
+  qsearch <- paste(c(qopen,"[^",qclose,"]+",qclose),collapse="")
+  iquotes <- gregexpr(qsearch,line)
+  startfirstquote <- iquotes[[1]][1]
+  trans <- stringr::str_extract(line,qsearch)
+  #cat(trans)
+  rest <- str_trim(stringr::str_sub(line,1,startfirstquote-1))
+  extra <- ""
+  if(length(iquotes[[1]])>1){
+    nextquote <- iquotes[[1]][2]
+    tail <- str_trim(stringr::str_sub(line,startfirstquote,nchar(line)))
+    endsearch <- paste(c("[^",qclose,"]",qclose),collapse="")
+    endfirst <- gregexpr(endsearch, tail)
+    endfirstquote <- endfirst[[1]][1]
+    newline <- str_trim(stringr::str_sub(tail,endfirst[[1]][1]+2,nchar(line)))
+    extra <- quotes(newline,qopen,qclose)
+  }
+  return(c(rest,trans,extra))
+}
+
+text <- readLines("~/scrapy/ralady/sources/def_out.txt")#read in text
+text <- stringr::str_replace(text,"\f","")
+empty <- grep("^$",text)
+text <- text[-empty]
+justno <- grep("^[ ]*[0-9]+[ ]*$",text)
+text <- text[-justno]
+text <- stringr::str_replace(text,"    ","")
+fulllines <- grep("^[aA-zZ]",text)
+text <- text[-fulllines]
+secs <- grep("^[0-9]",text)
+text <- text[-secs]
+twospace <- grep("^  [A-Z]",text)
+text <- text[-twospace]
+text <- str_trim(text)
+
+subs <- grep("^[a-z]\\.",text)
+starts <- grep("^\\([0-9]*\\)",text)
+cands <- union(subs,starts)
+cands <- sort(cands)
+for(i in 1:length(cands)){
+  curr <- cands[i]
+  if(i < length(cands)){
+    neigh <- cands[i+1]
+    if((neigh-curr)>8){
+      end <- curr+7
+    }
+    else{
+      if((neigh-curr)>2){
+        end <- neigh-1
+      }else{
+        if((neigh-curr)>1){
+          end <- curr+1
+        }else{
+          end <- curr
+        }
+      }
+    }
+    added <- text[curr:end]
+    text[curr]<- paste(added, sep=" ", collapse=" ")
+  }
+}
+all <- c(1:length(text))
+negs <- setdiff(all,cands)
+if(length(negs)>0){
+  text <- text[-negs]
+}
+
+text <- stringr::str_replace_all(text, "^\\([0-9]*\\)", "")
+text <- str_trim(text)
+text <- stringr::str_replace_all(text, "^[a-z]\\.", "")
+text <- str_trim(text)
+text <- stringr::str_replace(text,"^—","")
+text <- str_trim(text)
+quote <- grep("‘.*’",text)
+all <- c(1:length(text))
+noquote <- setdiff(all,quote)
+if(length(noquote)>0){
+  text <- text[-noquote]
+}
+ungrammatical <- grep("\\*",text)#remove ungrammatical sentences
+text <- text[-ungrammatical]
+splitt <- data.frame(Zap= character(0), Gloss=character(0), Trans=character(0),Lang=character(0),stringsAsFactors=FALSE)#create a dataframe
+for(i in 1:length(text)){
+  line <- text[i]
+  cat(line)
+  cat("\n")
+  full <- quotes(line,"‘","’")
+  indexes <- which(full == "")
+  if(length(indexes)>0){
+    full <- full[-indexes]
+  }
+  cat(length(full))
+  cat("\n")
+  for(p in 1:length(full)){
+    if(p%%2==1){
+      trans <- full[p+1]
+      rest <- full[p]
+      cat("We're in processing")
+      cat("\n")
+      cat(rest)
+      cat("\n")
+      cat(trans)
+      cat("\n")
+      z <- stringr::str_split(rest," ")[[1]]#split data/gloss on whitespace
+      indexes <- which(z == "")
+      indexes
+      if(length(indexes)>0){
+        z <- z[-indexes]
+      }
+      if((length(z)%%2)==0){#if there's more than 1 word, assume there is a gloss
+        len <- length(z)#calculate starting point of gloss
+        half <- floor(len/2)
+        zap <- str_trim(paste(z[1:half],collapse=" "))
+        gloss <- str_trim(paste(z[-(1:half)],collapse=" "))
+        splitt[i,] <- rbind(zap,gloss,trans,"SLQZ")
+      }else{
+        splitt[i,] <- rbind(str_trim(paste(z,collapse=" ")),"",trans,"SLQZ")
+      }
+    }
+  }
+}
+splitt <- splitt[rowSums(is.na(splitt)) != ncol(splitt),]
+write.table(splitt, "~/scrapy/ralady/cleaned_data/def_data.csv", sep="\t")
