a75e860261d48b798174277898f73db257d849a7
Carolyn Anderson <canders277@gmail.com>
2017-08-25 13:23:56 -0400

    simple scraping script

---
+++ scripts/tweets_simple.R
@@ -0,0 +1,14 @@
+library(streamR)
+library(ROAuth)
+
+setwd("~/Documents")
+load("my_oauth.Rdata")
+
+# Now set your working directory to the location where you would like to save
+# the tweets you are about to collect:
+setwd("~/scrapy/ralady/sources")
+
+filterStream("tweets.json",
+             track = c("#UsaTuVoz", "UsaTuVoz", "Zapoteco","#usatuvoz","Zapotec","todaslenguas","TodasLenguas"),
+             timeout = 10000,
+             oauth = my_oauth)
