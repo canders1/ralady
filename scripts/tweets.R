a5267d3df75d1e0798b2b1913612a3c862006162
Carolyn Anderson <canders277@gmail.com>
2017-10-05 16:23:00 -0400

    dump all text from tweets to all.txt

--- scripts/tweets.R
+++ scripts/tweets.R
@@ -5,3 +5,8 @@ library(dplyr)      # for %>% and other dplyr functions
 library(streamR)
 
 tweets.df <- parseTweets("~/scrapy/ralady/sources/tweets.json",simplify = TRUE)
+str <- ""
+for(line in tweets.df$text){
+  str <- paste0(str,line,sep="\n")
+}
+print(str)
